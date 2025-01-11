c055af71330d6e7bb4e97107b76610fb81728b98
Monika Grzanek <grzanekmonika@o2.pl>
2025-01-11 21:06:19 +0100

    Add state for columns and value, add handleSubmit method and add listener for the form in List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
 import styles from './List.module.scss';
 import Column from './../Column/Column';
+import { nanoid } from 'nanoid';
 
 const List = ()=> {
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
+    const [value, setValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setColumns([...columns, { id: nanoid(), title: value}]);
+        setValue('');
+    };
+  
     return(
         <div className={styles.list}>
             <header className={styles.header}>
@@ -9,10 +25,12 @@ const List = ()=> {
             </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <Column title='Books' icon='book' />
-                <Column title='Movies' icon='film' />
-                <Column title='Games' icon='gamepad' />
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
+            <form onSubmit={handleSubmit}>
+                <input type="text"  value={value} onChange={e => setValue(e.target.value)}/>
+                <button>Add column</button>
+            </form>
         </div>
     );
 }
