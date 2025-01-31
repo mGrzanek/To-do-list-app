24bc437801fc6a453e795a5fc51ac123cd85988c
Monika Grzanek <grzanekmonika@o2.pl>
2025-02-01 00:41:33 +0100

    Update Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,28 @@
 import styles from './Card.module.scss';
+import clsx from 'clsx';
+import { useDispatch } from 'react-redux';
+import { toggleCardFavorite } from '../../redux/store';
 import PropTypes from 'prop-types';
 
-const Card = ({ title }) => {
+const Card = ({ id, title, isFavorite }) => {
+    const dispatch = useDispatch();
+
+    const toggleFavorite = e => {
+        e.preventDefault();
+        dispatch(toggleCardFavorite(id));
+    }
     return(
-        <li className={styles.card}>{title}</li>
+        <li className={styles.card}>
+            {title}
+            <i className={clsx("fa fa-star-o", isFavorite && styles.isFavorite)} onClick={toggleFavorite} />
+        </li>
     );
 }
 
 Card.propTypes = {
-    title: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool.isRequired
 }
 
 export default Card;
