2f3347dc61c1a71865511851a26cf5f0eede0459
Monika Grzanek <grzanekmonika@o2.pl>
2025-01-31 11:26:16 +0100

    Add getColumnsByList selector to store and update List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,12 @@
 import styles from './List.module.scss';
 import { useSelector } from 'react-redux';
-import { getAllColumns, getListById } from '../../redux/store';
+import { getColumnsByList, getListById } from '../../redux/store';
 import Column from './../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
 
 const List = ()=> {
     const listData = useSelector(state => getListById(state, 1));
-    const columns = useSelector(getAllColumns);
+    const columns = useSelector(state => getColumnsByList(state, 1));
   
     return(
         <div className={styles.list}>
