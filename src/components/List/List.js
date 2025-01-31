4076ec792c763be532730b6e72312640d6051b29
Monika Grzanek <grzanekmonika@o2.pl>
2025-01-31 11:18:51 +0100

    Add getListById selector and update content List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,19 @@
 import styles from './List.module.scss';
 import { useSelector } from 'react-redux';
-import { getAllColumns } from '../../redux/store';
+import { getAllColumns, getListById } from '../../redux/store';
 import Column from './../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
 
 const List = ()=> {
+    const listData = useSelector(state => getListById(state, 1));
     const columns = useSelector(getAllColumns);
   
     return(
         <div className={styles.list}>
             <header className={styles.header}>
-                <h2 className={styles.title}>Things to do<span>soon</span></h2>
+                <h2 className={styles.title}>{listData.title}</h2>
             </header>
-            <p className={styles.description}>Interesting things I want to check out</p>
+            <p className={styles.description}>{listData.description}</p>
             <section className={styles.columns}>
                 {columns.map(column => <Column key={column.id} {...column} />)}
             </section>
